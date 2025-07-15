0c2bbb70ce221ce55e2b8c9a1112f5f9ccda66ba
Abdur-Rafay-AR <abdurrafay432007@gmail.com>
2025-07-15 18:31:51 +0500

    Add custom exceptions: ModelNotFound, ExperimentNotFound

--- src/runelog/__init__.py
+++ src/runelog/__init__.py
@@ -1,1 +1,19 @@
 from .tracker import Tracker
+from .exceptions import (
+    RunelogException,
+    ExperimentNotFound,
+    RunNotFound,
+    ModelNotFound,
+    ModelVersionNotFound,
+    ArtifactNotFound,
+    NoActiveRun,
+    InvalidExperimentId,
+    InvalidRunId,
+    RegistryError,
+    DuplicateModelRegistration,
+    InvalidModelVersion,
+    CorruptedMetadata,
+    InsufficientPermissions,
+    StorageError,
+    NoVersionsFound,
+)
